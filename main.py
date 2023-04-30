0245ea4e4641804da534d1c5bd33736106589e98
Alex <alexbolonkina@gmail.com>
2023-04-30 12:49:00 +0200

    add all pars moduls and training with github,igniring venv

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ from time import strftime
 import telebot
 from telebot import types
 
+#functions for parser
+from pars_module import cnn, bbc, meduza
+#bot token from hiden file
+from file_for_bot import bot_token
+
 #links
 cnn_europe = 'https://edition.cnn.com/world/europe'
 cnn_world = 'https://edition.cnn.com/world'
@@ -32,14 +37,16 @@ key_words = ['Russia','war ','nuclear ','Putin ','killed ','rocket','Duma','Pola
 #structure of scraping data
 Article = namedtuple("Article", "title link")
 
-#functions for parser
-from pars_package import pars_module
+#cnn_result function
+cnn_func = str(list((set(cnn(cnn_world) + cnn(cnn_europe)))))
+#bbc result function
+bbc_func = str(bbc(bbc_link))
+#meduza result function
+meduza_func = str(list(set(meduza(meduza_main))))
 
-cnn_func = list(set(pars_module.cnn(cnn_world) + pars_module.cnn(cnn_europe)))
 
-
-
-bot = telebot.TeleBot('6013995124:AAEtbHXLRDw7T3In3CZcKJMXxQWbDb33JiM')
+#BOT
+bot = telebot.TeleBot(bot_token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -75,56 +82,9 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, cnn_func, parse_mode = 'html')
 
     elif message.text =="BBC":
-        bot.send_message(message.from_user.id, "Here will be a parser", parse_mode = 'html')
+        bot.send_message(message.from_user.id, bbc_func, parse_mode = 'html')
 
     elif message.text =="Meduza":
-        bot.send_message(message.from_user.id, "Here will be a parser", parse_mode = 'html')    
+        bot.send_message(message.from_user.id, meduza_func, parse_mode = 'html')    
 
 bot.polling(non_stop=True)
-
-# @bot.message_handler(commands=['start'])
-# def website(message):
-#     bot.send_message(message.chat.id, "Do you want to know something new?",parse_mode = 'html')
-
-# @bot.message_handler(commands=['s'])
-# def start(message):
-#     mess = f'<b>Hello {message.from_user.first_name} { message.from_user.last_name}!</b> Do you want to know latest news? '
-#     #bot.send_message(message.chat.id, mess, parse_mode = 'html')
-    
-#     markup = types.InlineKeyboardMarkup()
-#     yes_answer = types.InlineKeyboardButton('YES')
-#     no_answer = types.InlineKeyboardButton('NO')
-#     markup.add(yes_answer,no_answer)
-#     bot.send_message(message.chat.id, mess, parse_mode = 'html', reply_markup=markup)
-
-#     if yes_answer:
-#         bot.reply_to(message,'Please choose the newspaper for downloading links')
-#     else:
-#         bot.reply_to(message,'It is seems like you do not want to know news about war...')
-
-
-# @bot.message_handler(func=lambda message: True)
-# def get_user_text(message):
-#  if message.text == 'hello' or message.text == 'hi':
-#      bot.send_message(message.chat.id, "Do you want to know latest news?", parse_mode = 'html')
-#  elif message.text == 'who are you?':
-#      bot.send_message(message.chat.id, "I am chatbot with latest news :)", parse_mode = 'html')
-#  else:
-#      bot.send_message(message.chat.id, "Do you want to know something new?",parse_mode = 'html')
-
-
-# @bot.message_handler(content_types=['text'])
-# def website(message):
-    
-#     if message.text == 'Original sourses':
-
-#         markup = types.InlineKeyboardMarkup()
-#         cnn_button_line = types.InlineKeyboardButton('CNN',url = 'https://edition.cnn.com/')
-#         bbc_button_line = types.InlineKeyboardButton('BBC', url = 'https://www.bbc.com/news')
-#         meduza_button_line = types.InlineKeyboardButton('Meduza', url = 'https://meduza.io/en')
-#         markup.add(cnn_button_line,bbc_button_line,meduza_button_line)
-#         bot.send_message(message.chat.id, 'Go to the newspaper website', reply_markup=markup)
-#     elif message.text =="CNN":
-#         bot.send_message(message.chat.id, "Here will be a parser", reply_markup=markup)
-
-
---
+++ pars_module.py
@@ -0,0 +1,103 @@
+import requests
+import bs4
+import lxml
+import re
+from collections import namedtuple
+
+#for scraping latest news
+from datetime import time,date,timedelta
+#working for today and yesterday
+
+#time structure
+today = date.today()
+yesterday = today - timedelta(days = 1)
+
+today = today.strftime("%Y/%m/%d")
+yesterday = yesterday.strftime("%Y/%m/%d")
+
+#key words for scraping
+key_words = ['Russia','war ','nuclear ','Putin ','killed ','rocket','Duma','Poland','Polish ','Duda ','Zelenski','Rada']
+
+#structure of scraping data
+Article = namedtuple("Article", "title link")
+
+#CNN
+
+def cnn(link):
+    articles_world = []
+    articles_europe = []
+    
+    res = requests.get(link)
+    soup = bs4.BeautifulSoup(res.content,'html.parser')
+    
+                  
+    for element in soup.find_all('a',class_="container__link container_lead-plus-headlines__link"):
+              
+        for key_word in key_words:
+            if key_word in element.text and (str(today) in element['href'] or str(yesterday) in element['href']):                 
+                articles_world.append(
+                    Article(
+                            title=element.text.replace('\n', '').replace('\xa0','').strip(),
+                            link='https://edition.cnn.com'+element['href']
+                    )
+                )  
+                
+
+    for element in soup.find_all('a',class_="container__link container_lead-plus-headlines-with-images__link"):
+            
+        for key_word in key_words:
+            if key_word in element.text and (str(today) in element['href'] or str(yesterday) in element['href']):                 
+                articles_europe.append(
+                    Article(
+                            title=element.text.replace('\n', '').replace('\xa0','').strip(),
+                            link='https://edition.cnn.com'+element['href']
+                    )
+                )  
+    
+    articles = articles_world + articles_europe
+    
+    return articles
+
+#BBC
+
+def bbc(link):
+
+    res = requests.get(link)
+    soup = bs4.BeautifulSoup(res.content,'html.parser')
+
+    articles = []
+
+    for element in soup.find_all('a',class_="qa-heading-link lx-stream-post__header-link"):
+        for key_word in key_words:
+             if key_word in element.text:
+                articles.append(
+                        Article(
+                                title=element.text,
+                                link='https://www.bbc.com'+element['href']
+                        )
+                )  
+
+    return list(set(articles))
+
+#MEDUZA
+
+def meduza(link):
+    articles = []
+    res = requests.get(link)
+    soup = bs4.BeautifulSoup(res.content,'html.parser')
+    
+           
+    for element in soup.find_all('a',class_="Link-root Link-isInBlockTitle"):
+            
+            for key_word in key_words:
+                if key_word in element.text and (str(today) in element['href'] or str(yesterday) in element['href']):                 
+                    articles.append(
+                        Article(
+                                title=element.text,
+                                link='https://meduza.io' + element['href']
+                        )
+                    )  
+    
+                  
+                
+    return articles
